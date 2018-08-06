51fa6598facc03d1c406db2785d34206327b0b6f
zvezdilin <artem@intervolga.ru>
2018-08-06 15:41:57 +0300

    дополнительные utilities

--- src/blocks.03-bootstrap/page/page.js
+++ src/blocks.03-bootstrap/page/page.js
@@ -1,3 +1,4 @@
+import 'bootstrap';
 // https://getbootstrap.com/docs/4.0/getting-started/browsers-devices/#internet-explorer-10-in-windows-phone-8
 // Copyright 2014-2017 The Bootstrap Authors
 // Copyright 2014-2017 Twitter, Inc.
@@ -5,9 +6,14 @@
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
   var msViewportStyle = document.createElement('style');
   msViewportStyle.appendChild(
-    document.createTextNode(
-      '@-ms-viewport{width:auto!important}'
-    )
+      document.createTextNode(
+          '@-ms-viewport{width:auto!important}'
+      )
   );
   document.head.appendChild(msViewportStyle);
 }
+if (!!window.$ || !!window.jQuery) {
+  console.warn('duplicate jQuery require');
+}
+window.$ = $;
+window.jQuery = jQuery;
